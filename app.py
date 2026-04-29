f4b2a5622795d733b512e2a795110fb07c46eda3
vanshri28 <vanshrimode96@gmail.com>
2026-04-29 23:41:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, session, jsonify
 import sqlite3
 import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 app = Flask(__name__)
 app.secret_key = "secret123"
 
