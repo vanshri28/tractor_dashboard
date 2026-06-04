c55e3db371471450bd0767fbdf39b05740b09ab9
vanshri28 <vanshrimode96@gmail.com>
2026-06-05 00:41:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -278,23 +278,53 @@ def db_test():
 
 @app.route("/update_plate", methods=["POST"])
 def update_plate():
+
     data = request.get_json()
     plate = data.get("plate")
+
     conn = get_connection()
-    
     cur = conn.cursor()
+
+    # latest row
     cur.execute("""
-    UPDATE entries
-    SET detected_number=%s
-    WHERE id = (
-        SELECT id
+        SELECT id, tractor
         FROM entries
         ORDER BY id DESC
         LIMIT 1
-    )
-    """, (plate,))
+    """)
+
+    row = cur.fetchone()
+
+    if row:
+
+        row_id = row[0]
+        tractor_number = row[1]
+
+        # detected plate save
+        cur.execute("""
+            UPDATE entries
+            SET detected_number=%s
+            WHERE id=%s
+        """, (plate, row_id))
+
+        # match check
+        if tractor_number == plate:
+
+            entry_no = "E" + str(random.randint(1000,9999))
+            token_no = "T" + str(random.randint(100,999))
+            current = datetime.now().strftime("%d-%m-%Y %I:%M:%S %p")
+
+            cur.execute("""
+                UPDATE entries
+                SET entry_no=%s,
+                    token=%s,
+                    time=%s
+                WHERE id=%s
+            """, (entry_no, token_no, current, row_id))
+
     conn.commit()
-    conn.close
+    conn.close()
+
     return jsonify({
         "status": "success",
         "plate": plate
